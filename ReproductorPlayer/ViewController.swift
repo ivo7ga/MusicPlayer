c0353431ce158e2cc57b5ae7ed246b6fbe820377
ivo7ga <ivan_gonzalez_tsapp1oa2021@cev.com>
2022-02-24 18:02:31 +0100

    Initial Commit

---
+++ ReproductorPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReproductorPlayer
+//
+//  Created by Iván González on 24/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
